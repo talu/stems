12d8c6e3a101d3175b3e8e8f89d036bab7835f3b
Mike Moulton <mike@meltmedia.com>
2016-01-06 16:49:46 -0700

    Updating defaults as Node > 0.12 have better handling of maxSockets

--- services/aws.js
+++ services/aws.js
@@ -10,20 +10,20 @@ var di = require('di'),
 
 var Aws = function(config) {
 
-  // Bump up the default of 5 so we can do a lot more work at once
-  http.globalAgent.maxSockets = 500;
-  https.globalAgent.maxSockets = 500;
-
+  // Bump up the default of 5 in Node < 0.12 so we can do a lot more work at once
+  if (http.globalAgent.maxSockets !== Infinity) {
+    http.globalAgent.maxSockets = Infinity;
+  }
+  if (https.globalAgent.maxSockets !== Infinity) {
+    https.globalAgent.maxSockets = Infinity;
+  }
 
   // Set global AWS configuration
   AWS.config.update({
     accessKeyId: config.get('aws:accessKey'),
     secretAccessKey: config.get('aws:secretKey'),
     region: config.get('aws:region'),
-    sslEnabled: true,
-    httpOptions: {
-      agent: https.globalAgent
-    }
+    sslEnabled: true
   });
 
   return AWS;
