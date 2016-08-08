463d1931df910d3439e4d5c1b159277d764bda63
Mike Moulton <mike@meltmedia.com>
2016-08-08 15:07:33 -0700

    Removing unnecessary dropIndex on db reset for test suite

--- test/fixtures/dataset.js
+++ test/fixtures/dataset.js
@@ -28,8 +28,7 @@ DatasetImporter.prototype.reset = function (done) {
     })
     .map((model) => {
       return async.series.bind(null, [
-        model.remove.bind(model),
-        model.collection.dropAllIndexes.bind(model.collection)
+        model.remove.bind(model)
       ]);
     });
 
